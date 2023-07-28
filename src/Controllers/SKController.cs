88015c4d2970d0f40cd42fd6a9f20d1251f41e31
Jim <17758778+JimPaine@users.noreply.github.com>
2023-07-28 12:46:11 +0100

    tweaked

--- src/Controllers/SKController.cs
+++ src/Controllers/SKController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Orchestration;
 using Microsoft.SemanticKernel.SemanticFunctions;
+using Microsoft.SemanticKernel.SkillDefinition;
 
 namespace src.Controllers;
 
@@ -10,25 +11,26 @@ namespace src.Controllers;
 public class SKController : ControllerBase {
 
     private readonly IKernel kernel;
+    private ISKFunction jokeFunction;
     public SKController(IKernel kernel) {
         this.kernel = kernel;
+
+        var promptConfig = PromptTemplateConfig.FromJson(System.IO.File.ReadAllText("./Skills/Joke/config.json"));
+        var template = new PromptTemplate(System.IO.File.ReadAllText("./Skills/Joke/skprompt.txt"), promptConfig, this.kernel.PromptTemplateEngine);
+        var function = new SemanticFunctionConfig(promptConfig, template);
+
+        this.jokeFunction = kernel.RegisterSemanticFunction("Joke", function);
     }
 
     [HttpGet(Name = "GetJoke/style/{style}/topic/{topic}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<string> Get(string style, string topic)
     {
-         var promptConfig = PromptTemplateConfig.FromJson(System.IO.File.ReadAllText("./Skills/Joke/config.json"));
-        var template = new PromptTemplate(System.IO.File.ReadAllText("./Skills/Joke/skprompt.txt"), promptConfig, this.kernel.PromptTemplateEngine);
-        var function = new SemanticFunctionConfig(promptConfig, template);
-
-        var func = kernel.RegisterSemanticFunction("Joke", function);
-
         var inputs = new ContextVariables();
         inputs.TryAdd("style", style);
         inputs.TryAdd("topic", topic);
 
-        var response = await kernel.RunAsync(inputs, func);
+        var response = await kernel.RunAsync(inputs, this.jokeFunction);
 
         return response.Result;
     }
